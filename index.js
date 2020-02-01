f835b711903916e786c2bac841f7e63c57580a5d
randyshao <shao.randy@gmail.com>
2020-02-01 10:16:42 -0500

    Basic web server

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("Hello from Nutribot!")
+//const functions = require("firebase-functions");
+//const admin = require("firebase-admin");
+//const serviceAccount = require("./service-account.json");
+//const request = require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const verifyWebhook = require('./verify-webhook');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', verifyWebhook);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+// const app_id = '29928df0'
+// const app_key = '1350d047cf8c640608deb36a4c9eec55'
+// // const url = `https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=${food}&app_id=${app_id}&app_key=${app_key}`
+// const url1 = `https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=red%20apple&app_id=29928df0&app_key=1350d047cf8c640608deb36a4c9eec55`
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://nutribot-huwsre.firebaseio.com"
+//   });
+
+//   const { SessionsClient } = require('dialogflow');
+
+
+//   exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+    
+//     const { queryInput, sessionId } = request.body;
+//     const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+//     const session = sessionClient.sessionPath('your-project', sessionId);
+  
+//     const responses = await sessionClient.detectIntent({ session, queryInput});
+//     const result = responses[0].queryResult;
+//     result.fulfillmentText
+//     response.send(result);
+
+//   });
+
+// request(url1, (error, response, body) => {
+//     const data = JSON.parse(body);
+//     console.log(data)
+// })
+
